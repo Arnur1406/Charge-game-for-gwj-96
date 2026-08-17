8addfdf2b92a1233b2acd2b062a587485639d240
Arnur <erbolatarnur6@gmail.com>
2026-08-17 21:48:03 +0500

    Added enemy #2 and changed a few scripts

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -8,3 +8,20 @@ class_name Enemy
 @export var enemy_charge = 0
 @onready var left_side_ray : RayCast2D
 @onready var right_side_ray : RayCast2D
+
+var direction : int = 1
+
+
+func _ready() -> void:
+	animated_sprite = $TemporaryTexture
+	right_side_ray = $RightSide
+	left_side_ray = $LeftSide
+
+
+
+func _process(delta: float) -> void:
+	position.x += delta * enemy_speed * direction
+	if not right_side_ray.is_colliding():
+		direction = -1
+	if not left_side_ray.is_colliding():
+		direction = 1
