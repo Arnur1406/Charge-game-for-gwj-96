ff79dd76ed8052dc19e6aa9d8d1092e1e3ec44f3
Arnur <erbolatarnur6@gmail.com>
2026-08-18 22:09:08 +0500

    Added a scene transition
    That's all

--- Globals or Autoloads/game_manager.gd
+++ Globals or Autoloads/game_manager.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@onready var tree = get_tree() 
+@onready var animation : AnimatedSprite2D = SceneTransition.find_child("AnimationForTransition")
 
 func switch_scenes(scene_name: int) -> void:
-	get_tree().change_scene_to_packed(Constants.SWITCHABLE_GAME_SCENES[scene_name])
+	SceneTransition.visible = true
+	animation.play("AnimationTransition")
+	await animation.animation_finished
+	tree.change_scene_to_packed(Constants.SWITCHABLE_GAME_SCENES[scene_name])
+	animation.play_backwards("AnimationTransition")
+	await animation.animation_finished
+	SceneTransition.visible = false
