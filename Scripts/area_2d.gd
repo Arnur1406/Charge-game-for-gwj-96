04a553c5af30d9d479b4836231f54c8193c92b6a
Arnur <erbolatarnur6@gmail.com>
2026-08-22 19:14:47 +0500

    started level 2

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -10,6 +10,7 @@ extends Area2D
 @onready var player : CharacterBody2D = get_tree().current_scene.find_child("Player")
 var did_collide : bool = false
 @onready var another_timer: Timer = $AnotherTimer
+@onready var third_timer: Timer = $ThirdTimer
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -20,6 +21,7 @@ func _on_body_entered(body: Node2D) -> void:
 		did_collide = true
 		player.velocity.y = 0
 		player.double_jump_available = false
+		third_timer.start()
 
 
 func _physics_process(delta: float) -> void:
@@ -37,3 +39,17 @@ func _on_area_entered(area: Area2D) -> void:
 		another_timer.start()
 		await another_timer.timeout
 		area.call_deferred("queue_free")
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	third_timer.stop()
+
+
+func _on_third_timer_timeout() -> void:
+		SignalHub.deal_damage.emit(damage, 0.0)
+		charge_bar.increment_value(energy, 0.0)
+		timer.start()
+		did_collide = true
+		player.velocity.y = 0
+		player.double_jump_available = false
+		third_timer.start()
